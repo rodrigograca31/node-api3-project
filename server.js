62b10d3f9f08f2b790b6098189cf065753d4bd35
rodrigograca31 <rodrigograca1993@gmail.com>
2020-01-15 12:26:26 +0000

    🎉 First commit

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require("express");
 
 const server = express();
 
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
+server.get("/", (req, res) => {
+	res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
